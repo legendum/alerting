55e3fc9b1be7b1fdb8ac215dc081e18dbf932e09
Kevin Hutchinson <legendum@gmail.com>
2026-05-30 17:40:09 -0600

    bun run lint:fix

--- src/web/components/WebhooksList.tsx
+++ src/web/components/WebhooksList.tsx
@@ -14,6 +14,7 @@ import {
   verticalListSortingStrategy,
 } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
+import { Logout } from "pues/base/auth";
 import {
   AddButton,
   Dialog,
@@ -26,7 +27,6 @@ import {
   useSwipeToReveal,
 } from "pues/base/objects";
 import { ThemeChooser } from "pues/base/theme";
-import { Logout } from "pues/base/auth";
 import { useCallback, useEffect, useState } from "react";
 import { formatMailHour } from "../formatMailHour";
 import { onEventsUpdate } from "../messages";
